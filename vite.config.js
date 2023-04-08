30b820953cd85953dfcfd42693989c2eb62951ad
Mohamed Abdi <mr.moxford@gmail.com>
2023-04-08 10:01:31 +0900

    adding peripherals

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// vite.config.js
+import svgr from "vite-plugin-svgr";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr()],
+});
