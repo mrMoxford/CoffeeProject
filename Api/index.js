355dc8183f4b7d7779da1e354bbfd96d2adf4db1
Mohamed Abdi <mr.moxford@gmail.com>
2023-04-13 18:47:13 +0900

    cors update

--- Api/index.js
+++ Api/index.js
@@ -9,6 +9,12 @@ const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/StripeCheckout");
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 const app = express();
 
 const mongoose = require("mongoose");
