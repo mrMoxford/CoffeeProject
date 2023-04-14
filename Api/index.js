1673e9766230d526ba53790e9534ef01f2c78525
Mohamed Abdi <mr.moxford@gmail.com>
2023-04-14 23:53:35 +0900

    heroku deploy 14-april

--- Api/index.js
+++ Api/index.js
@@ -14,13 +14,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hello world");
 });
-const corsOptions = {
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 const mongoose = require("mongoose");
 
 mongoose
